577d093b6219d74df40bc29b42d9da2fda7ddcf6
Darya Taghva <darya2000a0@gmail.com>
2024-05-12 07:54:35 -0800

    basic implementation of DownloadPage.js. not yet decided on how to manage the data.

--- src/page/model_generator/DownloadPage.js
+++ src/page/model_generator/DownloadPage.js
@@ -1,4 +1,11 @@
 const DownloadPage = () => {
+    const modelData = 'the bla bla file'
+    return(
+        <div>
+            <p>Your Model is ready</p>
+            <p>{modelData}</p>
+        </div>
+    )
 
 }
 export default DownloadPage;
